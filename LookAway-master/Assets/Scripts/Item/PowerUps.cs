5805eb812d014b53c592ba838912a39355251db9
Chaotique <vinidantasmaga06@gmail.com>
2019-10-13 17:13:20 -0300

    PowerUp Prototipo
    Versão bem simplificada de um power-up, ele apenas aumenta o tamanho do pulo, mas pode fazer outras coisas dependendo dos bools que adicionarmos posteriormente.

---
+++ LookAway-master/Assets/Scripts/Item/PowerUps.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUps : MonoBehaviour
+{
+    public bool HealthPU;
+    public bool JumpPU;
+    public GameObject player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if(collision.gameObject.tag == "Player")
+        {
+            PlayerEnhance(); //método a ser usado para mudar valores e buffar/ debuffar o player (Especialmente em movimentação livre)
+
+           Destroy(this.gameObject);
+            
+
+        }
+
+
+    }
+
+    private void PlayerEnhance()
+    {
+        if (JumpPU)
+        {
+            player.GetComponent<MoveChanPhisical>().jumpspeed = 20000;
+        }
+    }
+}
