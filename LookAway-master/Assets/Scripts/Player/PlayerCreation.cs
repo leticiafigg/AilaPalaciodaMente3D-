82c978b5a8c3c79988ccafdfbd070b5f8f137a00
Chaotique <vinidantasmaga06@gmail.com>
2019-11-05 00:39:10 -0200

    Adicionado Escolha de Classe
    Uma versão simples de escolha de arquétipo, que mdua os status a serem melhorados durante o jogo ao passar de nível. Adicionado um status novo : Resistência

--- LookAway-master/Assets/Scripts/Player/PlayerCreation.cs
+++ LookAway-master/Assets/Scripts/Player/PlayerCreation.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerCreation : MonoBehaviour
 {
@@ -10,30 +11,19 @@ public class PlayerCreation : MonoBehaviour
     private GameInformation informationAdd;
 
     private StatCalc statCalcScript = new StatCalc();
+    private string classDesc;
 
     private void Start()
     {
         newPlayer = new BasePlayer();
-
         newPlayer.PlayerName = "Aila";
         newPlayer.PlayerLevel = 1;
-        newPlayer.Poder = 15;
-        newPlayer.Imaginacao = 12;
-        newPlayer.Determinacao = 10;
-        newPlayer.Armadura = 0;
-        newPlayer.Sorte = 5;
-        newPlayer.XPAtual = 0;
-        newPlayer.XPNecessario = 300;
 
-        GameInformation.Aila = newPlayer;
-        GameInformation.AilaPV = statCalcScript.CalcularPV(GameInformation.Aila.Determinacao);
-        GameInformation.AilaPF = statCalcScript.CalcularPF(GameInformation.Aila.Imaginacao);
-        GameInformation.AilaPVatual = GameInformation.AilaPV;
-        GameInformation.AilaPFatual = GameInformation.AilaPF;
-        GameInformation.Aila.AilaClass = newPlayer.AilaArch(BasePlayer.AilaArchetype.CORAJOSA);
+
+
+
+
         
-
-        SaveInformation.SaveAll();
     }
 
     private void Update()
@@ -41,5 +31,84 @@ public class PlayerCreation : MonoBehaviour
         
     }
 
+    public void EscolherDestemida()
+    {
+        //usamos um new player temporário para depois salvá-lo no gameinformation
+        newPlayer.Poder = 15;   
+        newPlayer.Imaginacao = 8;
+        newPlayer.Resistencia = 13;
+        newPlayer.Determinacao = 10;
+        newPlayer.Sorte = 4;
+        newPlayer.Armadura = 0;
+        newPlayer.XPAtual = 0;
+        newPlayer.XPNecessario = 300;
+        newPlayer.AilaClass = BasePlayer.AilaArchetype.DESTEMIDA;
+
+        GameInformation.Aila = newPlayer;
+        //GameInformation.Aila.AilaClass = newPlayer.AilaClass;
+        GameInformation.AilaPV = statCalcScript.CalcularPV(GameInformation.Aila.Resistencia);
+        GameInformation.AilaPF = statCalcScript.CalcularPF(GameInformation.Aila.Imaginacao);
+        GameInformation.AilaPVatual = GameInformation.AilaPV;
+        GameInformation.AilaPFatual = GameInformation.AilaPF;
+       
+
+        classDesc = "Aila é especialmente corajosa e resistente a adversões. Ela se propões a encontrar de frente com problemas e inimigos (Maior Poder e Resistência)";
+    }
+
+    public void EscolherCriativa()
+    {
+        //usamos um new player temporário para depois salvá-lo no gameinformation
+        newPlayer.Poder = 8;
+        newPlayer.Imaginacao = 16;
+        newPlayer.Resistencia = 6;
+        newPlayer.Determinacao = 12;
+        newPlayer.Sorte = 6;
+        newPlayer.Armadura = 0;
+        newPlayer.XPAtual = 0;
+        newPlayer.XPNecessario = 300;
+        newPlayer.AilaClass = BasePlayer.AilaArchetype.CRIATIVA;
+
+        GameInformation.Aila = newPlayer;
+        
+        GameInformation.AilaPV = statCalcScript.CalcularPV(GameInformation.Aila.Determinacao);
+        GameInformation.AilaPF = statCalcScript.CalcularPF(GameInformation.Aila.Imaginacao);
+        GameInformation.AilaPVatual = GameInformation.AilaPV;
+        GameInformation.AilaPFatual = GameInformation.AilaPF;
+
+
+        classDesc = "Com o poder imaginativo da juventude, Aila é especialmente criativa, encontrado soluções menos óbvias para seus problemas (Maior Imaginação e Determinação) ";
+    }
+
+    public void EscolherAvoada()
+    {
+        //usamos um new player temporário para depois salvá-lo no gameinformation
+        newPlayer.Poder = 9;
+        newPlayer.Imaginacao = 9;
+        newPlayer.Resistencia = 10;
+        newPlayer.Determinacao = 10;
+        newPlayer.Sorte = 16;
+        newPlayer.Armadura = 0;
+        newPlayer.XPAtual = 0;
+        newPlayer.XPNecessario = 300;
+        newPlayer.AilaClass = BasePlayer.AilaArchetype.AVOADA;
+
+        GameInformation.Aila = newPlayer;
+        
+        GameInformation.AilaPV = statCalcScript.CalcularPV(GameInformation.Aila.Resistencia);
+        GameInformation.AilaPF = statCalcScript.CalcularPF(GameInformation.Aila.Imaginacao);
+        GameInformation.AilaPVatual = GameInformation.AilaPV;
+        GameInformation.AilaPFatual = GameInformation.AilaPF;
+
+
+        classDesc = "Aila permite que alguns pontos mais banais sejam decididos pelo destino, não se abala demais quando as coisas dão errado 'Não era pra ser' (Maior Sorte e status equilibrados) ";
+    }
+
+    public void ConfirmarDefinitivo()
+    {
+
+        SaveInformation.SaveAll();
+
+        SceneManager.LoadScene("mapa1");
+    }
 
 }
