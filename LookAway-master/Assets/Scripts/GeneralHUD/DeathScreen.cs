bc3262fef53f05d9c424a5febe4f0d6f0fdcd8cf
ChaotiqueVolpe <42809398+ChaotiqueVolpe@users.noreply.github.com>
2019-11-12 11:59:34 -0300

    Revert "Safety merge"

---
+++ LookAway-master/Assets/Scripts/GeneralHUD/DeathScreen.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScreen : MonoBehaviour
+{
+    public GameObject deathScreenObj;
+    public GameObject playerObj;
+    public GameObject baseHudObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameInformation.AilaPVatual <= 0)
+        {
+            deathScreenObj.SetActive(true);
+            playerObj.SetActive(false); //desativa o jogador para que ele não possa fazer mais nada 
+            baseHudObj.SetActive(false);
+        }
+    }
+
+    public void CarregarSave()
+    {
+        LoadInformation.LoadAll();
+        if (GameInformation.LastScene != null && GameInformation.LastScene != "")
+        {
+            SceneManager.LoadScene(GameInformation.LastScene);
+        }
+    }
+
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+}
