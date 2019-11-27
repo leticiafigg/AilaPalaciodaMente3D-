e42be836ef8aae8d95f78bb21f73d32a28925405
Chaotique <vinidantasmaga06@gmail.com>
2019-11-27 02:28:04 -0200

    Ver0.8
    Novo Mapa, musica da primeira cena, e iluminação

--- LookAway-master/Assets/Scripts/Battling/Actions/Shove.cs
+++ LookAway-master/Assets/Scripts/Battling/Actions/Shove.cs
@@ -7,8 +7,8 @@ public class Shove : BaseAction
         ActionName = "Empurrar";
         ActionDesc = "Avança com a intenção de desequilibrar o oponente. Dano reduzido, mas com maior Atordoamento";
         ActionID = 2;
-        ActionPower = 2;
-        StunPower = 30;
+        ActionPower = 5;
+        StunPower = 40;
         ActionCost = 0;
         ActionCritChance = 35;
 
